65dea9491a76957a6b5feb4d4f81ca4e955f6666
Aginas pk <aginaspk6@gmail.com>
2025-01-29 11:50:26 +0530

    update

---
+++ src/controllers/user/cartController.js
@@ -0,0 +1,71 @@
+import cart from "../../models/schema/cartSchema";
+
+const getUserCart = async (req, res) => {
+  const cartItems = await cart.findOne({ userId: req.user.id }).populate({
+    path: "products.productId",
+    select: "name price images",
+  });
+  if (cartItems) {
+    res.status(200).json({
+      status: "success",
+      data,
+    });
+  } else {
+    res.status(200).json({
+      status: "success",
+      data: {
+        products: [],
+      },
+    });
+  }
+};
+
+const updateUserCart = async (req, res) => {
+  const { productId, quantity } = req.body;
+
+  let cartItems = await cart.findOne({ userId: req.user.id });
+
+  if (!cartItems) {
+    cartItems = new cart({
+      userId: req.user.id,
+      products: [{ productId, quantity }],
+    });
+  } else {
+    const productIndex = cartItems.products.findIndex(
+      (product) => product.productId.toString() === productId.toString()
+    );
+    if (productIndex > -1) {
+      cartItems.products[productIndex].quantity += quantity;
+    } else {
+      cartItems.products.push({ productId, quantity });
+    }
+  }
+  const cartItemsSaved = await cartItems.save();
+  await cartItemsSaved.populate({
+    path: "products.productId",
+    select: "name price images",
+  });
+
+  res.status(200).json({
+    message: "cart updated",
+  });
+};
+
+const removeFromCart = async (req, res) => {
+  const cartItem = await cart.findOne(
+    {
+      userId: req.user.id,
+      "products.productId": req.body.productId,
+    },
+    { $pull: { products: { productId: req.body.productId } } },
+    { new: true }
+  );
+
+  if (cartItem) {
+    res.status(200).json({ message: "product removed from cart" });
+  } else {
+    res.status(404).json({ message: "cart not found" });
+  }
+};
+
+module.exports = { getUserCart, updateUserCart, removeFromCart };
